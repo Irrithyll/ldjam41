c887036e200f67fdcfcc6dabc20802edbfd3d175
Tim <timmy_g2@hotmail.com>
2018-04-21 16:27:36 +1000

    updated person prefab, game script

--- ldjam41/Assets/Scripts/Game.cs
+++ ldjam41/Assets/Scripts/Game.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class Game : MonoBehaviour {
 
-    float timer;
+    private float timer;
 
-	// Use this for initialization
-	void Start () {
-        timer = 0.0f;
+    public float timerDuration = 1000.0f;
+    public float randomTimerDurationRange = 100.0f;
+
+    public GameObject personSpawnPrefab;
+    public Transform personSpawnLocaltion;
+
+    // Use this for initialization
+    void Start () {
+        resetTimer();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if (timer <= 0.0f) {
             // timer finished
+            resetTimer();
             doTheThing();
         } else {
             // countdown timer
@@ -22,10 +29,14 @@ public class Game : MonoBehaviour {
 	}
 
     void doTheThing() {
-
+        Instantiate(personSpawnPrefab, personSpawnLocaltion);
     }
 
     void countDownTimer() {
+        timer -= Time.deltaTime;
+    }
 
+    void resetTimer() {
+        timer = Random.Range(timerDuration - randomTimerDurationRange, timerDuration + randomTimerDurationRange);
     }
 }
