b741e774624e1aa48715f683a9ca1746275107fc
Sarah Giapitzakis <sarah.giapitzakis@gmail.com>
2018-04-21 14:55:22 +1000

    Added main game screen and basic player movement
    Added main game screen and basic player movement

--- ldjam41/Assets/PlayerController.cs
+++ ldjam41/Assets/PlayerController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+     public float walkSpeed = 15f;
+     private float curSpeed;
 
-	// Use this for initialization
 	void Start () {
-		
+ 
+	}
+
+	void FixedUpdate() {
+		curSpeed = walkSpeed;
+
+		float x = Mathf.Lerp(0, Input.GetAxis("Horizontal")* curSpeed, 0.8f);
+		float y = Mathf.Lerp(0, Input.GetAxis("Vertical")* curSpeed, 0.8f);
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(x,y);
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
