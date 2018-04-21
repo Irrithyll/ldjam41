901572c22a7fe92923d1daa8c86f179c891bcab3
Tim <timmy_g2@hotmail.com>
2018-04-21 15:35:20 +1000

    added person prefab

---
+++ ldjam41/Assets/Scripts/Game.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (timer <= 0.0f) {
+            // timer finished
+            doTheThing();
+        } else {
+            // countdown timer
+            countDownTimer();
+        }
+	}
+
+    void doTheThing() {
+
+    }
+
+    void countDownTimer() {
+
+    }
+}
